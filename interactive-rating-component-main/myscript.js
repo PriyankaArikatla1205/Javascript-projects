da3fb366892a5f47b63f22803d0934c137b659c4
Priyanka Reddy <90396220+PriyankaArikatla1205@users.noreply.github.com>
2023-06-05 20:00:40 +0530

    Add files via upload

---
+++ interactive-rating-component-main/myscript.js
@@ -0,0 +1,12 @@
+
+let value =0;
+function inc(){
+    value+=1;
+    console.log(value);
+ document.getElementById("ans").innerHTML=value;
+
+}
+function submit(){
+   document.getElementById("cont").style.visibility="hidden";
+   document.getElementById("cont1").style.visibility="visible";
+}
